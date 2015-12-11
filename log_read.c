6a316164469777bc5b9e2b113823f943cec8f67b
liujinye <liujinye@xiaomi.com>
2015-12-11 08:59:38 +0800

    Merge changes I598c8c5c,I672b0f4d,Id882978b,Idff5e080,Ib124eca1, ... am: e09d581770
    * commit 'e09d581770ff615663499e877b1442de14e8eb49':
      logger.h: reduce maximum payload so sum is page size
      liblog: test for maximum payload can not survive change
      liblog: logprint use uid name if length less then 5
      liblog: logprint add uid format modifier
      liblog: readlog apps get logger_entry_v4 for pstore
      logd: readlog apps get logger_entry_v4
      logger.h: Add definition for logger_entry_v4

--- log_read.c
+++ log_read.c
@@ -208,7 +208,6 @@ static const char *LOG_NAME[LOG_ID_MAX] = {
     [LOG_ID_EVENTS] = "events",
     [LOG_ID_SYSTEM] = "system",
     [LOG_ID_CRASH] = "crash",
-    [LOG_ID_SECURITY] = "security",
     [LOG_ID_KERNEL] = "kernel",
 };
 
