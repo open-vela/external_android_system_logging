90a83a18683fd54f4045a38bfee215a1b1720173
openvela-robot <openvela-robot@xiaomi.com>
2016-04-14 15:31:56 +0800

    liblog: android_log_isloggable failing apct
    (cherry pick from commit efe8ecc1d963452f0ed650756a968a7befa71c57)
    
    - periodic failures in apct, dropped second serial test
      in refresh_cache, trusting check_cache or global.
    - The retry loop to see if is_loggable recovers of 1000
      was hiding subsequent tests, drop to 10 retries.
    - On the whole, the average performance remains the same.
    
    Bug: 25792367
    Change-Id: I4110440ef46671d7a1c128689bde623808bed04f

--- logprint.c
+++ logprint.c
@@ -21,7 +21,6 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
-#include <pwd.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -29,11 +28,11 @@
 #include <string.h>
 #include <inttypes.h>
 #include <sys/param.h>
-#include <sys/types.h>
 
 #include <cutils/list.h>
 #include <log/logd.h>
 #include <log/logprint.h>
+#include <private/android_filesystem_config.h>
 
 #include "log_portability.h"
 
@@ -1353,17 +1352,17 @@ LIBLOG_ABI_PUBLIC char *android_log_formatLogLine (
     uid[0] = '\0';
     if (p_format->uid_output) {
         if (entry->uid >= 0) {
+            const struct android_id_info *info = android_ids;
+            size_t i;
 
-            /*
-             * This code is Android specific, bionic guarantees that
-             * calls to non-reentrant getpwuid() are thread safe.
-             */
-#ifndef __BIONIC__
-#warning "This code assumes that getpwuid is thread safe, only true with Bionic!"
-#endif
-            struct passwd* pwd = getpwuid(entry->uid);
-            if (pwd && (strlen(pwd->pw_name) <= 5)) {
-                 snprintf(uid, sizeof(uid), "%5s:", pwd->pw_name);
+            for (i = 0; i < android_id_count; ++i) {
+                if (info->aid == (unsigned int)entry->uid) {
+                    break;
+                }
+                ++info;
+            }
+            if ((i < android_id_count) && (strlen(info->name) <= 5)) {
+                 snprintf(uid, sizeof(uid), "%5s:", info->name);
             } else {
                  // Not worth parsing package list, names all longer than 5
                  snprintf(uid, sizeof(uid), "%5d:", entry->uid);
