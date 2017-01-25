2a12ed9757dd8d0e482187ec8ee7818de2bf3309
openvela-robot <openvela-robot@xiaomi.com>
2017-01-25 23:48:01 +0800

    Merge "Revert "Revert "Exporting C headers from system/core""" am: 01678a0c84 am: e6588b4bfa am: 4a720cecf1 am: af73945086
    Change-Id: I4d63b6e6d4949fb06d60abf755cc9443fd46c23f

--- include_vndk/log/log.h
+++ include_vndk/log/log.h
@@ -4,9 +4,10 @@
 #define _LIBS_LOG_LOG_H
 
 #include <android/log.h>
-#include <log/log_id.h>
-#include <log/log_main.h>
-#include <log/log_radio.h>
+
+/*The following files will be included once they are available*/
+/*#include <log/log_id.h>*/
+/*#include <log/log_radio.h>*/
 
 /*
  * LOG_TAG is the local tag used for the following simplified
