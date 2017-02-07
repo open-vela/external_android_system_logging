143d429877fa02144c296b7577f7854901807391
openvela-robot <openvela-robot@xiaomi.com>
2017-02-08 00:03:28 +0800

    Merge "liblog: logger.h fortify comments" am: 0145bb4e79 am: a4b0fd51f8 am: 98cf53fa39 am: c7785266d8
    Change-Id: I2edbc1cf665ce256e92659054cc3370a8938c9ea

--- pmsg_writer.c
+++ pmsg_writer.c
@@ -285,7 +285,6 @@ LIBLOG_ABI_PRIVATE ssize_t __android_log_pmsg_file_write(
                 __android_log_unlock();
             } else if (pmsgOpen() < 0) {
                 __android_log_unlock();
-                free(cp);
                 return -EBADF;
             }
         }
