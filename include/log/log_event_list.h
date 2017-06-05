cda4eda5e41aa01270dabc2fd6378ab4f9f93327
liujinye <liujinye@xiaomi.com>
2017-06-05 14:39:25 +0800

    add component metadata tag to CTS liblog test acses
    Bug: 36140955
    Bug: 35360169
    Test: new unit test to ensure that all modules have this field
    Change-Id: I352145965e8cd20371ccce3ba2b25f3a166bc3e8
    Merged-In: Ifb7a14817265b4624f1dc6414b547c86aafcf4ac

--- include/log/log_event_list.h
+++ include/log/log_event_list.h
@@ -17,7 +17,6 @@
 #ifndef _LIBS_LOG_EVENT_LIST_H
 #define _LIBS_LOG_EVENT_LIST_H
 
-#include <errno.h>
 #include <stdint.h>
 
 #if (defined(__cplusplus) && defined(_USING_LIBCXX))
@@ -149,7 +148,6 @@ class android_log_event_list {
     return ctx;
   }
 
-  /* return errors or transmit status */
   int status() const {
     return ret;
   }
@@ -173,12 +171,6 @@ class android_log_event_list {
 
   android_log_event_list& operator<<(uint32_t value) {
     int retval = android_log_write_int32(ctx, static_cast<int32_t>(value));
-    if (retval < 0) ret = retval;
-    return *this;
-  }
-
-  android_log_event_list& operator<<(bool value) {
-    int retval = android_log_write_int32(ctx, value ? 1 : 0);
     if (retval < 0) ret = retval;
     return *this;
   }
@@ -217,16 +209,14 @@ class android_log_event_list {
   }
 
   int write(log_id_t id = LOG_ID_EVENTS) {
-    /* facilitate -EBUSY retry */
-    if ((ret == -EBUSY) || (ret > 0)) ret = 0;
     int retval = android_log_write_list(ctx, id);
-    /* existing errors trump transmission errors */
-    if (!ret) ret = retval;
+    if (retval < 0) ret = retval;
     return ret;
   }
 
   int operator<<(log_id_t id) {
-    write(id);
+    int retval = android_log_write_list(ctx, id);
+    if (retval < 0) ret = retval;
     android_log_destroy(&ctx);
     return ret;
   }
--- tests/liblog_test.cpp
+++ tests/liblog_test.cpp
@@ -1720,7 +1720,6 @@ TEST(liblog, is_loggable) {
 // Kills logd and toss all collected data, equivalent to logcat -b all -c,
 // except we also return errors to the logging callers.
 #ifdef USING_LOGGER_DEFAULT
-#ifdef __ANDROID__
 #ifdef TEST_PREFIX
 // helper to liblog.enoent to count end-to-end matching logging messages.
 static int count_matching_ts(log_time ts) {
@@ -1833,8 +1832,7 @@ TEST(liblog, enoent) {
   GTEST_LOG_(INFO) << "This test does nothing.\n";
 #endif
 }
-#endif  // __ANDROID__
-#endif  // USING_LOGGER_DEFAULT
+#endif  // USING_LOCAL_LOGD
 
 // Below this point we run risks of setuid(AID_SYSTEM) which may affect others.
 
