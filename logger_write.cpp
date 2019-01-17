e673ff60c5c78626fb8026807eaa4d9c3faf9233
openvela-robot <openvela-robot@xiaomi.com>
2019-01-17 09:29:51 +0800

    Merge "Remove __android_log_event_list and the reader aspect of android_log_event_list" am: e926f1f8f1
    Change-Id: Id421a042d7dcc83d6c70311a726dd677af4886c8

--- logger_write.cpp
+++ logger_write.cpp
@@ -33,7 +33,6 @@
 #include "config_write.h"
 #include "log_portability.h"
 #include "logger.h"
-#include "uio.h"
 
 #define LOG_BUF_SIZE 1024
 
@@ -525,8 +524,11 @@ LIBLOG_ABI_PUBLIC void __android_log_assert(const char* cond, const char* tag, c
 
   // Log assertion failures to stderr for the benefit of "adb shell" users
   // and gtests (http://b/23675822).
-  TEMP_FAILURE_RETRY(write(2, buf, strlen(buf)));
-  TEMP_FAILURE_RETRY(write(2, "\n", 1));
+  struct iovec iov[2] = {
+      {buf, strlen(buf)},
+      {(char*)"\n", 1},
+  };
+  TEMP_FAILURE_RETRY(writev(2, iov, 2));
 
   __android_log_write(ANDROID_LOG_FATAL, tag, buf);
   abort(); /* abort so we have a chance to debug the situation */
