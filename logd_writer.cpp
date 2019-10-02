a113f173214f57969123f79a44a72ca3a7ba37f7
liujinye <liujinye@xiaomi.com>
2019-10-02 10:52:55 +0800

    liblog: don't set transports to nullptr when they close
    The old code did not free the transports, just unlink their nodes from
    the linked list, so there was no race condition between close() and
    other threads writing to the logs.  By settings these to nullptr, I
    introduced a race condition, but setting them to nullptr isn't
    necessary, so let's simply not do that.
    
    Test: liblog-unit-tests
    Change-Id: Iec0b680addd13b5d30bd91ccef5bdeab6bf797b0

--- logd_writer.cpp
+++ logd_writer.cpp
@@ -38,6 +38,9 @@
 #include "logger.h"
 #include "uio.h"
 
+/* branchless on many architectures. */
+#define min(x, y) ((y) ^ (((x) ^ (y)) & -((x) < (y))))
+
 static int logdAvailable(log_id_t LogId);
 static int logdOpen();
 static void logdClose();
