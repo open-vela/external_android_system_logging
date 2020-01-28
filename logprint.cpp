f9f4ef84b015e083389cdb2b316abfb11b7b4174
liujinye <liujinye@xiaomi.com>
2020-01-29 04:23:14 +0800

    Snap for 6166993 from 123ccf0ceeb601b39f693e612aa245d60b0d3506 to rvc-release
    Change-Id: I665914b547e60e595dc46d55ba9b15ced01cc0fd

--- logprint.cpp
+++ logprint.cpp
@@ -1520,7 +1520,12 @@ char* android_log_formatLogLine(AndroidLogFormat* p_format, char* defaultBuffer,
  * This code is Android specific, bionic guarantees that
  * calls to non-reentrant getpwuid() are thread safe.
  */
-#ifdef __ANDROID__
+#if !defined(__MINGW32__)
+#if (FAKE_LOG_DEVICE == 0)
+#ifndef __BIONIC__
+#warning "This code assumes that getpwuid is thread safe, only true with Bionic!"
+#endif
+#endif
       struct passwd* pwd = getpwuid(entry->uid);
       if (pwd && (strlen(pwd->pw_name) <= 5)) {
         snprintf(uid, sizeof(uid), "%5s:", pwd->pw_name);
